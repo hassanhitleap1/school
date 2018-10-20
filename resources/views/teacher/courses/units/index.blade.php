631cb9c66db8cb592531ff46091de7e0d4a8ec3f
hasan kiwan <hassanhitleap1@gmail.com>
2018-10-20 11:18:31 -0700

    educef

--- resources/views/teacher/courses/units/index.blade.php
+++ resources/views/teacher/courses/units/index.blade.php
@@ -16,6 +16,7 @@
     <section class="content">
         <div class="row">
             <div class="col-md-12">
+            <a href="{{url('teacher/courses')}}" class="btn btn-primary btn-lg" >Back </a>
                 <a href="{{url('teacher/courses/'.$id.'/units/create')}}" class="btn btn-primary btn-lg" >Create </a>
             </div>
         </div>
