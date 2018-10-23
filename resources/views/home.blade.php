d38f33db5bcafe39ab3a014e6b19b95da486d538
hasan kiwan <hassanhitleap1@gmail.com>
2018-10-23 14:42:24 -0700

    layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        @foreach ($coures as $coure)
-            <div class="col-md-3">
-                <h5>{{$coure->name}}</h5>
-                <p>{{$coure->description}}</p>
-                <h5>{{$coure->teacher->name}}</h5>
-            </div>
-        @endforeach
+    
 
     </div>
 </div>
