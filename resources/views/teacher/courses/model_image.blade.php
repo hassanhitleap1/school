7341584b30b915de3ed85417b394fb2036be7c3b
hasan <hassanhitleap1@gmail.com>
2018-10-24 17:27:11 +0300

    select image

--- resources/views/teacher/courses/model_image.blade.php
+++ resources/views/teacher/courses/model_image.blade.php
@@ -1,7 +1,68 @@
 <div class="row">
     @foreach ($covers as $cover)
-        <div class="col-md-3">
-            <img src="{{asset($cover->cover_path_help)}}" style="width: 200px; height: 200px"/>
-        </div>
+    <div class="col-xs-12 col-sm-6 col-md-3 nopad text-center">
+            <label class="image-checkbox">
+              <img src="{{asset($cover->cover_path_help)}}" style="width: 200px; height: 200px" />
+              <input type="checkbox" name="image" value="" />
+              <i class="fa fa-check hidden"></i>
+            </label>
+          </div>
     @endforeach
 </div>
+<style>
+
+.nopad {
+	padding-left: 0 !important;
+	padding-right: 0 !important;
+}
+/*image gallery*/
+.image-checkbox {
+	cursor: pointer;
+	box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	-webkit-box-sizing: border-box;
+	border: 4px solid transparent;
+	margin-bottom: 0;
+	outline: 0;
+}
+.image-checkbox input[type="checkbox"] {
+	display: none;
+}
+
+.image-checkbox-checked {
+	border-color: #4783B0;
+}
+.image-checkbox .fa {
+  position: absolute;
+  color: #4A79A3;
+  background-color: #fff;
+  padding: 10px;
+  top: 0;
+  right: 0;
+}
+.image-checkbox-checked .fa {
+  display: block !important;
+}
+</style>
+
+<script>
+// image gallery
+// init the state from the input
+$(".image-checkbox").each(function () {
+  if ($(this).find('input[type="checkbox"]').first().attr("checked")) {
+    $(this).addClass('image-checkbox-checked');
+  }
+  else {
+    $(this).removeClass('image-checkbox-checked');
+  }
+});
+
+// sync the state to the input
+$(".image-checkbox").on("click", function (e) {
+  $(this).toggleClass('image-checkbox-checked');
+  var $checkbox = $(this).find('input[type="checkbox"]');
+  $checkbox.prop("checked",!$checkbox.prop("checked"))
+
+  e.preventDefault();
+});
+</script>
